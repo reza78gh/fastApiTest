bfa2502e1c66870292e6658e9674841b8d187290
rezag <rezaghanbarii2000@gmail.com>
2025-11-15 18:36:50 +0330

    add new route

--- main.py
+++ main.py
@@ -11,3 +11,9 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+@app.get("/bye/{name}/{city}")
+async def say_hello(name: str, city: str):
+    return {"message": f"goodbye {name} from {city}"}
+
