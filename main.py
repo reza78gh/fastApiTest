e8814affbd36bf370dcda470dbc5c82ccb65ecde
rezag <rezaghanbarii2000@gmail.com>
2025-11-15 18:42:58 +0330

    add mr befor name

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ async def root():
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+    return {"message": f"Hello mr {name}"}
 
 
 @app.get("/bye/{name}/{city}")
 async def say_hello(name: str, city: str):
-    return {"message": f"goodbye {name} from {city}"}
+    return {"message": f"goodbye mr {name} from {city}"}
 
