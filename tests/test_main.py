54132313b6ea59326d4fdd31dd24a0534de88ca6
rezag <rezaghanbarii2000@gmail.com>
2025-11-16 11:32:07 +0330

    complete test for bye user

--- tests/test_main.py
+++ tests/test_main.py
@@ -13,4 +13,10 @@ def test_root():
 def test_hello_user():
     response = client.get('/hello/reza')
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello reza"}
+    assert response.json() == {"message": "Hello mr reza"}
+
+
+def test_bye_user():
+    response = client.get('/bye/reza/qom')
+    assert response.status_code == 200
+    assert response.json() == {"message": "goodbye mr reza from qom"}
